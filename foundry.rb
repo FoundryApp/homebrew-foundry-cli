c5b418d6653d315538300b6b34f2af62324081c8
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2020-04-15 10:56:13 +0200

    Add foundry.rb formulae [WIP]

---
+++ foundry.rb
@@ -0,0 +1,5 @@
+class Foundry < Formula
+  desc "A command-line tool that lets you build Firebase Cloud Functions notably faster, with less configuration, and with easy access to your production data"
+  homepage "https://github.com/FoundryApp/foundry-cli"
+  url ""
+end
