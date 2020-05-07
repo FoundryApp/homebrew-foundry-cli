f20597aee569db6cc6d504c3bbd4ffa79be8a8d8
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2020-05-07 20:34:33 +0200

    Fix checksum

--- foundry.rb
+++ foundry.rb
@@ -2,7 +2,7 @@ class Foundry < Formula
   desc "A command-line tool that lets you build Firebase Cloud Functions notably faster, with less configuration, and with easy access to your production data"
   homepage "https://github.com/FoundryApp/foundry-cli"
   url "https://github.com/FoundryApp/foundry-cli/releases/download/v0.2.4/foundry-macos-x86_64"
-  sha256 "34c8c45c51ee0fb46498d772cfb8b0f9b1d0687e53d79107b61ce41bd67ad899"
+  sha256 "d96e0dbc94f480ca34c6f7d723bb3bee9714774dd10080b7a4773d773c07135d"
 
   bottle :unneeded
   
