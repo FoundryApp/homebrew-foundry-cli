8a6c082814b3d4f8730a83b982b95381dfb1abba
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2020-05-07 20:21:44 +0200

    Version bump

--- foundry.rb
+++ foundry.rb
@@ -1,8 +1,8 @@
 class Foundry < Formula
   desc "A command-line tool that lets you build Firebase Cloud Functions notably faster, with less configuration, and with easy access to your production data"
   homepage "https://github.com/FoundryApp/foundry-cli"
-  url "https://github.com/FoundryApp/foundry-cli/releases/download/v0.2.3/foundry-macos-x86_64"
-  sha256 "8ed780b979e7791fb60c729b9b663599d38edd2b4081b5a0d9da424426f4feb9"
+  url "https://github.com/FoundryApp/foundry-cli/releases/download/v0.2.4/foundry-macos-x86_64"
+  sha256 "34c8c45c51ee0fb46498d772cfb8b0f9b1d0687e53d79107b61ce41bd67ad899"
 
   bottle :unneeded
   
